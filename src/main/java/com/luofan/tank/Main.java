c7c6bd3ca95ab1f7e6f0e2234ad2fe12d7f440d8
mark-fan <15289340589@163.com>
2021-03-26 18:01:16 +0800

    抽象出坦克类相应的抽象方法，在frame中进行使用

--- src/main/java/com/luofan/tank/Main.java
+++ src/main/java/com/luofan/tank/Main.java
@@ -10,9 +10,10 @@ import java.awt.event.WindowEvent;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         TankFrame tankFrame = new TankFrame();
-        while (true){
+        while (true) {
             Thread.sleep(50);
             tankFrame.repaint();
         }
+
     }
 }
